6cfbda044d24b9a86a388d7c80c18c3c8bca73d3
lucas bianchi <lucmbianchi2000@gmail.com>
2024-01-08 02:15:37 -0300

    first commit

---
+++ taskapi/src/app.module.ts
@@ -0,0 +1,23 @@
+// import { Module } from '@nestjs/common';
+// import { TasksModule } from './tasks/tasks.module';
+// import { MongooseModule } from '@nestjs/mongoose';
+
+// @Module({
+//   imports: [MongooseModule.forRoot('mongodb://localhost/tasksdb'), TasksModule],
+// })
+// export class AppModule {}
+import { Module } from '@nestjs/common';
+import { TasksModule } from './tasks/tasks.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    TasksModule,
+    MongooseModule.forRoot('mongodb://localhost:27017/nestjs'),
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
